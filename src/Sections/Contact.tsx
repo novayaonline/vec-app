2266f86903f48780ca708f6151d3d260e09f8f79
Felipe Maifredo <felipemaifredo@gmail.com>
2023-08-22 21:38:13 -0300

    att

--- src/Sections/Contact.tsx
+++ src/Sections/Contact.tsx
@@ -1,10 +1,68 @@
 //Imports
 import './Styles/contact.style.css';
+import { useState } from 'react';
+
+//Configs
+const linkToFetch = 'https://formsubmit.co/felipemaifredo@gmail.com';
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        text: ''
+    });
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        if ( !formData.name || !formData.email || !formData.subject || !formData.text ) {
+            alert('erro')
+          } else {
+            fetch(linkToFetch, {
+              method: 'post',
+              headers: { 'Accept': 'aplication/json', 'Content-Type': 'application/json' },
+              body: JSON.stringify({ 
+                _captcha: 'false',
+                _subject: 'Novo Contato no Site!',
+                Nome: formData.name,
+                Email: formData.email,
+                Assunto: formData.subject,
+                Texto: formData.text
+              })
+            }).then(() => {
+              alert('Mensagem enviada')
+              setTimeout(() => {
+                clearForm();
+              }, 5000);
+            });
+        }; 
+    };
+
+    const clearForm = () => {
+        setFormData({
+            name: '',
+            email: '',
+            subject: '',
+            text: '',
+        });
+    };
+
     return (
         <section id="contact">
-            <h1>Hello Contact</h1>
+            <p>Possui alguma dúvida?</p>
+            <h2>Não hesite em entrar em contato conosco, preencha o formulário abaixo.</h2>
+            <form id='form-contact' onSubmit={ sendEmail }  >
+                <div className='form-nameandemail'>
+                    <input type='text' placeholder='Nome' value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+                    <input type='text' placeholder='Email' value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                </div>
+                <input type='text' placeholder='Assunto' value={formData.subject} onChange={(e) => setFormData({ ...formData, subject: e.target.value })} />
+                <textarea placeholder='Digite sua mensagem' value={formData.text} onChange={(e) => setFormData({ ...formData, text: e.target.value })} ></textarea>
+                <div className='btn-box-form'>
+                    <button type='submit' id='formBtn' className='button-style'>Enviar</button>
+                </div>
+            </form>
         </section>
     )
 }
